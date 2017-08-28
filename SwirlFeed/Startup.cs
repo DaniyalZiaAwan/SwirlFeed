84499973e4d119181caab21b3a6983c3e8e22b4f
DaniAwan <danizia.awan@gmail.com>
2017-08-28 11:53:55 -0700

    Add project files.

---
+++ SwirlFeed/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SwirlFeed.Startup))]
+namespace SwirlFeed
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
